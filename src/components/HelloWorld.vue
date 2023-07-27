fe2d5d1563726c7ca085b236620d272e001ca642
joren97x <babamyloves2002@gmail.com>
2023-07-27 11:52:51 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,84 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+<script setup>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import {ref} from 'vue'
+    import axios from 'axios'
+
+    const link = ref('')
+    const shortenedLink = ref('')
+    const loading = ref(false)
+    const token = '7ed9b44598093c5a723c5907d662e54376fedcd0' //YOUR TOKEN //my token => 7ed9b44598093c5a723c5907d662e54376fedcd0
+    const guid = 'Bn7r266WM7y' //GROUP_GUID // my guid => Bn7r266WM7y
+
+    async function validURL() {
+        loading.value = true
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        const valid = !!pattern.test(link.value);
+        if(valid) {
+
+            const url = 'https://api-ssl.bitly.com/v4/shorten'
+            const data = {
+                "group_guid": `${guid}`,
+                "domain": "bit.ly",
+                "long_url": link.value
+            }  
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': "application/json"
+                }
+            }
+
+            await axios.post(url, data, config)
+            .then(result => {
+                shortenedLink.value = result.data.link
+                loading.value = false
+            })
+            .catch(err => {
+                shortenedLink.value = err.message
+                loading.value = false
+            })
+
+            // const result = fetch('https://api-ssl.bitly.com/v4/shorten', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Authorization': `Bearer ${token}`,
+            //         'Content-Type': 'application/json'
+            //     },
+            //     body: JSON.stringify({ "long_url": "https://dev.bitly.com", "domain": "bit.ly", "group_guid": "o_4sh2f1psh2" })
+            // });
+            // console.log(result)
+        }
+    }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<template>
+    <v-container>
+        <v-text-field label="Enter a URL" class="mt-5" prepend-inner-icon="mdi-link" v-model="link" @keydown.enter="validURL()">
+
+        </v-text-field>
+        <v-row class="justify-center">
+            <v-btn color="green" @click="validURL()">Shorten</v-btn>
+            
+        </v-row>
+        <v-container>
+            <h3> {{ shortenedLink }} </h3>
+        <v-row>
+            <v-col cols="5">
+                <v-progress-linear
+            color="deep-purple-accent-4"
+            indeterminate
+            rounded
+            v-show="loading"
+            height="3"
+          ></v-progress-linear>
+            </v-col>
+        </v-row>
+        </v-container>
+</v-container>
+</template>
